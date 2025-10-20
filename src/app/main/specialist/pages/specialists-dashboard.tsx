8ce58b6e98949b2edcd4e421e5e8dfc4f5ac2e77
AyScript <olasupoayomide557@gmail.com>
2025-10-20 14:43:39 +0100

    added modal to specialist

--- src/app/main/specialist/pages/specialists-dashboard.tsx
+++ src/app/main/specialist/pages/specialists-dashboard.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import PageHeader from "../../components/page-header";
 import SpecialistCard from "../components/specialist-card";
 import { specialists } from "../libs/constants";
+import Button from "@/components/ui/button";
+import { ArrowDownUp, Search, SlidersHorizontal } from "lucide-react";
+import FormInput from "@/components/ui/input";
+import Modal from "@/components/ui/modal/modal";
 
 const SpecialistsPage: React.FC = () => {
+  const [isFilterModalOpen, setFilterModalOpen] = React.useState(false);
+
   return (
     <>
       <PageHeader
@@ -12,12 +18,41 @@ const SpecialistsPage: React.FC = () => {
       />
 
       {/* Filter bar */}
-      <section className="h-32 bg-white/20 rounded-xl"></section>
+      <section className="h-20 py-2 px-4 bg-white/20 rounded-xl flex items-end justify-between">
+        <div className="w-fit flex xl:gap-4 sm:gap-2 gap-1">
+          <Button
+            text="Filter"
+            onClick={() => setFilterModalOpen(true)}
+            startAdornment={<SlidersHorizontal size={20} />}
+          />
+
+          <Button text="Sort" startAdornment={<ArrowDownUp size={20} />} />
+        </div>
+
+        <div className="hidden xl:block">
+          <FormInput
+            type="search"
+            placeholder="Search for a specialist..."
+            id="search"
+            icon={<Search size={20} />}
+          />
+        </div>
+      </section>
 
       <main className="bg-white/20 h-[95%] overflow-y-auto rounded-2xl xl:p-8 lg:p-6 md:p-4 p-3 grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-6 auto-rows-min">
-        {specialists.slice(0, 14).map((specialist) => (
+        {specialists.slice(0, 8).map((specialist) => (
           <SpecialistCard key={specialist.id} {...specialist} />
         ))}
+
+        <Modal
+          isOpen={isFilterModalOpen}
+          title="Filter Specialists"
+          onClose={() => {
+            setFilterModalOpen(false);
+          }}
+        >
+          <p>Filter Specialists</p>
+        </Modal>
       </main>
     </>
   );
---
+++ src/components/ui/modal/modal.tsx
@@ -0,0 +1,35 @@
+import { X } from "lucide-react";
+
+interface ModalProps {
+  title: string;
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ title, children, isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 h-screen flex items-center justify-center">
+      {/* overlay */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      {/* modal content */}
+      <div className="relative bg-white p-8 rounded-lg shadow-lg z-10">
+        <button
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 cursor-pointer"
+          onClick={onClose}
+        >
+          <X size={20} />
+        </button>
+
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
